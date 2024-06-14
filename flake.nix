fa715fdc909ff9fe2f495d5dd09a4e0967f80d8d
Luís Eduardo Batista <luiseduardob303@gmail.com>
2024-06-14 11:58:48 -0300

    Fix: bugfix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/unstable";
-    home-manager.url = "github:nix-community/home-manager/unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -29,7 +29,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "luisb@${hostname}" = home-manager.lib.homeManagerConfiguration {
+      "luisb" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
