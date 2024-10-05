615c6b3d7d6f83a7264bc378b367416d1aca90a3
luiseduardobatista <luiseduardob303@gmail.com>
2024-10-04 22:32:27 -0300

    Feat: Moved gnome settings from configuration.nix to its own module

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./gnome.nix
     ];
 
   # Bootloader.
@@ -38,10 +39,6 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # OpenGL
   hardware.opengl.enable = true;
@@ -112,7 +109,6 @@
   	vim
   	git
   	curl
-	gnomeExtensions.pop-shell
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
