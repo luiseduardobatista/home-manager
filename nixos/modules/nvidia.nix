9f483d528e4eafe1f94f4d9f4809d1756c0bed64
luiseduardobatista <luiseduardob303@gmail.com>
2024-10-04 22:42:18 -0300

    Moved nvidia settings to separated module and set zsh as default on configuration.nix

---
+++ nixos/modules/nvidia.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.services.nvidia;
+in
+
+{
+  options.services.nvidia.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Enable NVIDIA settings.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.xserver.videoDrivers = ["nvidia"];
+    hardware.nvidia.modesetting.enable = true;
+    hardware.nvidia.open = true;
+  };
+}
+
