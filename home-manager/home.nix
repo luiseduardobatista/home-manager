3483621a68ce9d6c9eb156b61934c7961cf6063d
luiseduardobatista <luiseduardob303@gmail.com>
2024-10-13 17:47:49 -0300

    Idk

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,19 +12,19 @@
   targets.genericLinux.enable = true;
 
   imports = [
-    ./zsh.nix
-    ./neovim.nix
-    ./devtools.nix
-    ./programming-languages.nix
-    ./gnome/gnome.nix
+     ./zsh.nix
+     ./neovim.nix
+     ./devtools.nix
+     ./programming-languages.nix
+     ./gnome/gnome.nix
     # ./gnome/pop-os.nix
-    ./gui.nix
-    ./utils.nix
-    ./git.nix
-    ./ssh.nix
-    ./cli.nix
-    ./fonts.nix
-    ./hyprland.nix
+    # ./gui.nix
+     ./utils.nix
+     ./git.nix
+     ./ssh.nix
+     ./cli.nix
+     ./fonts.nix
+    # ./hyprland.nix
     (builtins.fetchurl {
       url = "https://raw.githubusercontent.com/Smona/home-manager/nixgl-compat/modules/misc/nixgl.nix";
       sha256 = "f14874544414b9f6b068cfb8c19d2054825b8531f827ec292c2b0ecc5376b305";
