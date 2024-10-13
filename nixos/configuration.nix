e44d7b8da28b2aa25139c3a51b564612b8b924fe
luiseduardobatista <luiseduardob303@gmail.com>
2024-10-12 23:23:48 -0300

    Fix: Fix configuration.nix bugs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,27 +1,36 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
 }: {
   imports = [
+    # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ./modules/gnome.nix
     ./modules/nvidia.nix
     ./modules/zsh.nix
+    ./modules/gnome.nix
   ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  networking.wireless.enable = false; # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
 
   # Select internationalisation properties.
@@ -98,25 +107,19 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # Enable Binaries
+  programs.nix-ld.enable = true;
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  nix.settings.experimental-features = ["nix-command" "flakes"];
   environment.systemPackages = with pkgs; [
     vim
+    wget
     git
-    curl
   ];
-
-  environment.sessionVariables = {
-    TERMINAL = "wezterm";
-    TERM = "wezterm";
-  };
-
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -129,7 +132,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
