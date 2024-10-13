580abc9608ca92cc6b6d7b849a533d549169b806
luiseduardobatista <luiseduardob303@gmail.com>
2024-10-12 23:25:11 -0300

    Feat: Update home-manager gnome settings

--- home-manager/gnome/gnome.nix
+++ home-manager/gnome/gnome.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     ./pop-shell-config.nix
+    ./shared.nix
   ];
 
   home.packages = with pkgs; [
@@ -18,17 +19,11 @@
     gnomeExtensions.dash-to-dock
     gnomeExtensions.vitals
     gnomeExtensions.blur-my-shell
-    gnomeExtensions.user-themes
     gnomeExtensions.space-bar
     gnomeExtensions.just-perfection
     gnomeExtensions.alphabetical-app-grid
     gnomeExtensions.astra-monitor
-
-    # Utils
-    gnome.dconf-editor
-    gnome-extensions-cli # Para pegar os nome completo da extensão
-    gnome.sushi
-    gnome.gnome-tweaks
+    gnomeExtensions.tray-icons-reloaded
 
     # Themes
     tokyonight-gtk-theme
@@ -42,33 +37,24 @@
         "dash-to-dock@micxgx.gmail.com"
         "vitals@CoreCoding.com"
         "blur-my-shell@aunetx"
-        "user-theme@gnome-shell-extensions.gcampax.github.com"
         "space-bar@luchrioh"
         "just-perfection-desktop@just-perfection"
         "monitor@astraext.github.io"
         "AlphabeticalAppGrid@stuarthayhurst"
+        "trayIconsReloaded@selfmade.pl"
       ];
     };
 
     # Gnome
-    "org/gnome/mutter" = {
-      dynamic-workspaces = false;
-      center-new-windows = true;
-    };
-
-    "org/gnome/desktop/wm/preferences" = {
-      num-workspaces = 6;
-    };
-
     "org/gnome/desktop/calendar" = {
       "show-weekdate" = true;
     };
 
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      binding = "<Super>t";
-      command = "${config.home.sessionVariables.TERMINAL}";
-      name = "Launch Terminal";
-    };
+    # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+    #   binding = "<Super>t";
+    #   command = "${config.home.sessionVariables.TERMINAL}";
+    #   name = "Launch Terminal";
+    # };
 
     #Pop Shell bindings
     "org/gnome/desktop/wm/keybindings" = {
@@ -131,40 +117,6 @@
       rotate-video-lock-static = [];
     };
 
-    "org/gnome/desktop/wm/keybindings" = {
-      move-to-workspace-1 = ["<Super><Shift>1"];
-      move-to-workspace-2 = ["<Super><Shift>2"];
-      move-to-workspace-3 = ["<Super><Shift>3"];
-      move-to-workspace-4 = ["<Super><Shift>4"];
-      move-to-workspace-5 = ["<Super><Shift>5"];
-      move-to-workspace-6 = ["<Super><Shift>6"];
-      move-to-workspace-7 = ["<Super><Shift>7"];
-      move-to-workspace-8 = ["<Super><Shift>8"];
-      move-to-workspace-9 = ["<Super><Shift>9"];
-
-      switch-to-workspace-1 = ["<Super>1"];
-      switch-to-workspace-2 = ["<Super>2"];
-      switch-to-workspace-3 = ["<Super>3"];
-      switch-to-workspace-4 = ["<Super>4"];
-      switch-to-workspace-5 = ["<Super>5"];
-      switch-to-workspace-6 = ["<Super>6"];
-      switch-to-workspace-7 = ["<Super>7"];
-      switch-to-workspace-8 = ["<Super>8"];
-      switch-to-workspace-9 = ["<Super>9"];
-    };
-
-    "org/gnome/shell/keybindings" = {
-      switch-to-application-1 = [];
-      switch-to-application-2 = [];
-      switch-to-application-3 = [];
-      switch-to-application-4 = [];
-      switch-to-application-5 = [];
-      switch-to-application-6 = [];
-      switch-to-application-7 = [];
-      switch-to-application-8 = [];
-      switch-to-application-9 = [];
-    };
-
     # Blur My Shell
     "org/gnome/shell/extensions/blur-my-shell" = {
       "dash-to-dock/blur" = false;
@@ -173,6 +125,8 @@
     # Dash to Dock
     "org/gnome/shell/extensions/dash-to-dock" = {
       hot-keys = false;
+      custom-theme-shrink = true;
+      running-indicator-style = "DOTS";
     };
 
     # Space Bar
--- home-manager/gnome/pop-os.nix
+++ home-manager/gnome/pop-os.nix
@@ -4,75 +4,23 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [
-    # Extensions
-    gnomeExtensions.user-themes
-    gnomeExtensions.bluetooth-quick-connect
-    gnomeExtensions.sound-output-device-chooser
-
-    # Utils
-    gnome.dconf-editor
-    gnome-extensions-cli # Para pegar os nome completo da extensão
-    gnome.sushi
-    gnome.gnome-tweaks
+  imports = [
+    ./shared.nix
   ];
 
-  programs.gnome.wallpaper = {
-    file = "./background.png";
-  };
+  home.packages = with pkgs; [
+    # Extensions
+    gnomeExtensions.bluetooth-quick-connect
+    gnomeExtensions.sound-output-device-chooser
+  ];
 
   dconf.settings = {
     # Enable Extensions
     "org/gnome/shell" = {
       "enabled-extensions" = [
-        "user-theme@gnome-shell-extensions.gcampax.github.com"
         "bluetooth-quick-connect@bjarosze.gmail.com"
         "sound-output-device-chooser@kgshank.net"
       ];
-    };
-
-    # Gnome
-    "org/gnome/mutter" = {
-      dynamic-workspaces = false;
-      center-new-windows = true;
-    };
-
-    "org/gnome/desktop/wm/preferences" = {
-      num-workspaces = 8;
-    };
-
-    "org/gnome/shell/keybindings" = {
-      switch-to-application-1 = [];
-      switch-to-application-2 = [];
-      switch-to-application-3 = [];
-      switch-to-application-4 = [];
-      switch-to-application-5 = [];
-      switch-to-application-6 = [];
-      switch-to-application-7 = [];
-      switch-to-application-8 = [];
-      switch-to-application-9 = [];
-    };
-
-    "org/gnome/desktop/wm/keybindings" = {
-      move-to-workspace-1 = ["<Super><Shift>1"];
-      move-to-workspace-2 = ["<Super><Shift>2"];
-      move-to-workspace-3 = ["<Super><Shift>3"];
-      move-to-workspace-4 = ["<Super><Shift>4"];
-      move-to-workspace-5 = ["<Super><Shift>5"];
-      move-to-workspace-6 = ["<Super><Shift>6"];
-      move-to-workspace-7 = ["<Super><Shift>7"];
-      move-to-workspace-8 = ["<Super><Shift>8"];
-      move-to-workspace-9 = ["<Super><Shift>9"];
-
-      switch-to-workspace-1 = ["<Super>1"];
-      switch-to-workspace-2 = ["<Super>2"];
-      switch-to-workspace-3 = ["<Super>3"];
-      switch-to-workspace-4 = ["<Super>4"];
-      switch-to-workspace-5 = ["<Super>5"];
-      switch-to-workspace-6 = ["<Super>6"];
-      switch-to-workspace-7 = ["<Super>7"];
-      switch-to-workspace-8 = ["<Super>8"];
-      switch-to-workspace-9 = ["<Super>9"];
     };
   };
 }
