9e6050f0284f06b56b112b896b6b7a0dfc531467
luiseduardobatista <luiseduardob303@gmail.com>
2024-10-12 23:24:15 -0300

    Fix: Remove unecessary code on nixos module gnome

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -5,9 +5,4 @@
 }: {
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    gnomeExtensions.pop-shell
-    gnomeExtensions.blur-my-shell
-  ];
 }
