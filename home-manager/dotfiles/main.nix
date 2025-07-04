a6d0d0205f1c6ba3add0b192975472895804545d
Luís Eduardo Batista <luisb@kyros.com.br>
2025-07-04 10:40:06 -0300

    feat(dotfiles): add foot terminal config

--- home-manager/dotfiles/main.nix
+++ home-manager/dotfiles/main.nix
@@ -5,6 +5,7 @@
 }: {
   home.file = {
     ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink (toString ./wezterm);
+    ".config/foot".source = config.lib.file.mkOutOfStoreSymlink (toString ./foot);
     ".config/nvim-teste".source = inputs.lazyvim;
   };
 }
