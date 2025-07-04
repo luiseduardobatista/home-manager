d628c6e2aebbe7611b630764abec72ad41e9b017
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-07-04 11:10:31 -0300

    feat(dotfiles): add kitty config

--- home-manager/dotfiles/main.nix
+++ home-manager/dotfiles/main.nix
@@ -6,6 +6,7 @@
   home.file = {
     ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink (toString ./wezterm);
     ".config/foot".source = config.lib.file.mkOutOfStoreSymlink (toString ./foot);
+    ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink (toString ./kitty);
     ".config/nvim-teste".source = inputs.lazyvim;
   };
 }
