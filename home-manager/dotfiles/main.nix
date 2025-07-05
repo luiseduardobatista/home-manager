563a3f46ed737bd674877394f6a2997f3eade0b5
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-07-05 20:15:18 -0300

    feat(dotfiles): add neovim config

--- home-manager/dotfiles/main.nix
+++ home-manager/dotfiles/main.nix
@@ -7,6 +7,6 @@
     ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink (toString ./wezterm);
     ".config/foot".source = config.lib.file.mkOutOfStoreSymlink (toString ./foot);
     ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink (toString ./kitty);
-    ".config/nvim-teste".source = inputs.lazyvim;
+    ".config/nvim".source = inputs.lazyvim;
   };
 }
