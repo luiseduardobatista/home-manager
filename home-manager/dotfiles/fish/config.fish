0c3e824202640b89389adea282a9ac179db0a987
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-07-08 18:13:52 -0300

    fix(dotfiles): remove old gemini api key from fish config

--- home-manager/dotfiles/fish/config.fish
+++ home-manager/dotfiles/fish/config.fish
@@ -11,5 +11,5 @@ alias hmc 'sudo nix-collect-garbage -d; nix-collect-garbage -d'
 alias hms 'home-manager switch --flake .'
 alias fhmu 'nix flake update && home-manager switch --flake .'
 
-export GEMINI_API_KEY="AIzaSyA8uDrOUD3a6XU_--WqQ8dayvnvyHEtNHA"
+export GEMINI_API_KEY=""
 export SSH_AUTH_SOCK=~/.1password/agent.sock
