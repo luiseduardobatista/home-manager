3e79cdf7c68486fb0ca27a6b7f68540902e6a8b9
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-07-21 15:12:06 -0300

    feat: add sesh integration with fish

--- home-manager/dotfiles/fish/config.fish
+++ home-manager/dotfiles/fish/config.fish
@@ -4,6 +4,26 @@ end
 
 set fish_greeting
 
+function sesh_interactive
+    set -l session (sesh list --icons | fzf --height 90% --layout=reverse \
+        --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
+        --header '  C-a All | C-t Tmux | C-g Configs | C-x Zoxide | C-f Find | C-d Kill Session' \
+        --bind 'tab:down,btab:up' \
+        --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+        --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+        --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+        --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+        --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+        --preview-window 'right:50%' \
+        --preview 'sesh preview {}' < /dev/tty)
+    commandline -f repaint >/dev/null 2>&1
+    if test -z "$session"
+        return
+    end
+    set -l session_name (string split -f 2 ' ' "$session")
+    sesh connect "$session_name"
+end
+
 alias zj zellij
 alias vim nvim
 alias ls eza
@@ -13,5 +33,6 @@ alias hmc 'sudo nix-collect-garbage -d; nix-collect-garbage -d'
 alias hms 'home-manager switch --flake .'
 alias fhmu 'nix flake update && home-manager switch --flake .'
 alias testdox='gotestsum --format=testdox'
+bind \cf sesh_interactive
 
 export SSH_AUTH_SOCK=~/.1password/agent.sock
