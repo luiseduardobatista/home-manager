a34fe42618f4cd7a236c34e882ded4f49102dc83
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-01-04 14:23:01 -0300

    feat: add nixos settings

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "My flake config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixGL = {
       url = "github:nix-community/nixGL";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,7 +27,6 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    hostname = "nixos";
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
@@ -45,6 +46,63 @@
         modules = [
           nix-flatpak.homeManagerModules.nix-flatpak
           ./home-manager/home.nix
+        ];
+      };
+    };
+
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./nixos/hosts/desktop/configuration.nix
+          ./nixos/common.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = {
+              inherit inputs outputs;
+              inherit nixGL;
+              inherit nix-flatpak;
+              isNixOS = true;
+            };
+            home-manager.users.luisb = {
+              imports = [
+                ./home-manager/home.nix
+                nix-flatpak.homeManagerModules.nix-flatpak
+              ];
+            };
+          }
+        ];
+      };
+
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./nixos/hosts/laptop/configuration.nix
+          ./nixos/common.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = {
+              inherit inputs outputs;
+              inherit nixGL;
+              inherit nix-flatpak;
+              isNixOS = true;
+            };
+
+            home-manager.users.luisb = {
+              imports = [
+                ./home-manager/home.nix
+                nix-flatpak.homeManagerModules.nix-flatpak
+              ];
+            };
+          }
         ];
       };
     };
---
+++ nixos/hosts/desktop/configuration.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [./hardware-configuration.nix];
+  networking.hostName = "desktop";
+
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    modesetting.enable = true;
+    powerManagement.enable = false;
+  };
+}
