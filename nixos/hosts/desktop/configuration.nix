5842c7e55a3770b437635335bc3db498cdd113e2
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-01-06 14:01:54 -0300

    fix: fix DE graphical glitch when nvidia pc returns from sleep

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -2,11 +2,12 @@
   config,
   pkgs,
   ...
-}: {
-  imports = [./hardware-configuration.nix];
+}:
+{
+  imports = [ ./hardware-configuration.nix ];
   networking.hostName = "desktop";
 
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.graphics.enable = true;
   hardware.nvidia = {
     open = false;
@@ -15,4 +16,8 @@
     modesetting.enable = true;
     powerManagement.enable = true;
   };
+
+  systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+  systemd.services.systemd-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+  systemd.services.systemd-hybrid-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
 }
