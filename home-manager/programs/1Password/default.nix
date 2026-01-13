72ec34ec0f36ac2738fdb1d8fc4e2fedede8038a
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-01-13 07:55:59 -0300

    fix(1Password): move agent.toml to correct level

--- home-manager/programs/1Password/default.nix
+++ home-manager/programs/1Password/default.nix
@@ -1,3 +1,19 @@
-{linkApp, ...}: {
+{
+  lib,
+  pkgs,
+  linkApp,
+  isNixOS,
+  ...
+}:
+{
   xdg.configFile."1Password/ssh" = linkApp "1Password";
+  xdg.configFile."autostart/1password.desktop" = lib.mkIf isNixOS {
+    text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=1Password
+      Exec=${pkgs._1password-gui}/bin/1password --silent
+      Terminal=false
+    '';
+  };
 }
--- home-manager/programs/autostart/default.nix
+++ home-manager/programs/autostart/default.nix
@@ -3,14 +3,6 @@
   lib,
   isNixOS,
   ...
-}: {
-  xdg.configFile."autostart/1password.desktop" = lib.mkIf isNixOS {
-    text = ''
-      [Desktop Entry]
-      Type=Application
-      Name=1Password
-      Exec=${pkgs._1password-gui}/bin/1password --silent
-      Terminal=false
-    '';
-  };
+}:
+{
 }
