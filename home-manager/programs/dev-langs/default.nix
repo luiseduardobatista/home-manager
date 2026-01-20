9e923008215220cf6dd298cfe6c990b8e88fa785
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-01-20 07:48:24 -0300

    feat(langs): add script to auto install rustup toolchain

--- home-manager/programs/dev-langs/default.nix
+++ home-manager/programs/dev-langs/default.nix
@@ -12,4 +12,15 @@
       python3
       go
     ];
+
+  home.activation = {
+    installRustStable = lib.hm.dag.entryAfter ["installPackages"] ''
+      if ! ${pkgs.rustup}/bin/rustup default >/dev/null 2>&1; then
+        echo "--- Configurando Rustup: Instalando toolchain stable como padrão ---"
+        ${pkgs.rustup}/bin/rustup default stable
+      else
+        echo "--- Rustup: Toolchain stable já está configurado. Pulando... ---"
+      fi
+    '';
+  };
 }
