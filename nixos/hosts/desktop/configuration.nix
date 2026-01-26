cd8a30328858ceb39283b91053b69b0c9d73ebc4
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-01-26 07:51:54 -0300

    fix(niri): High VRAM usage fix on Nvidia

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -24,4 +24,29 @@
   ];
 
   programs.niri.enable = true;
+
+  environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text = ''
+    {
+      "rules": [
+        {
+          "pattern": {
+            "feature": "procname",
+            "matches": "niri"
+          },
+          "profile": "Limit Free Buffer Pool On Wayland Compositors"
+        }
+      ],
+      "profiles": [
+        {
+          "name": "Limit Free Buffer Pool On Wayland Compositors",
+          "settings": [
+            {
+              "key": "GLVidHeapReuseRatio",
+              "value": 0
+            }
+          ]
+        }
+      ]
+    }
+  '';
 }
