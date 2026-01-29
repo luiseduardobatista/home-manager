5ecc21cc87384fba2ce7a9b1f7637e1c32969f13
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-01-29 16:37:26 -0300

    feat(niri): add swayidle config

--- home-manager/sessions/niri/default.nix
+++ home-manager/sessions/niri/default.nix
@@ -10,4 +10,29 @@
     fuzzel
   ];
   xdg.configFile."niri" = linkFile "sessions/niri/config";
+
+  services.swayidle = {
+    enable = true;
+    systemdTarget = "graphical-session.target";
+
+    timeouts = [
+      {
+        timeout = 300;
+        command = "noctalia-shell ipc call lockScreen lock";
+      }
+      {
+        timeout = 600;
+        command = "noctalia-shell ipc call sessionMenu lockAndSuspend";
+      }
+    ];
+
+    events = [
+      {
+        event = "before-sleep";
+        command = "noctalia-shell ipc call lockScreen lock";
+      }
+    ];
+  };
+
+  systemd.user.services.swayidle.Unit.ConditionEnvironment = lib.mkForce "XDG_CURRENT_DESKTOP=niri";
 }
