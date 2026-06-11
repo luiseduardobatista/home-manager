1b445a4c4c5335b397572e51470b35631a7796cb
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-06-11 15:39:23 -0300

    feat(pi): auto-clone my pi settings from github on rebuild

--- home-manager/programs/pi/default.nix
+++ home-manager/programs/pi/default.nix
@@ -1,6 +1,29 @@
-{linkApp, ...}: {
+{
+  pkgs-unstable,
+  lib,
+  config,
+  ...
+}: {
   programs.pi.coding-agent = {
     enable = true;
   };
-  # xdg.configFile."mise" = linkApp "mise";
+
+  home.activation.clonePiDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    #!/usr/bin/env bash
+    set -euo pipefail
+    PI_DOTFILES_DIR="${config.home.homeDirectory}/.pi/agent"
+    if [ ! -d "$PI_DOTFILES_DIR/.git" ]; then
+      echo "Clonando o repositório PI em $PI_DOTFILES_DIR..."
+      # Clonar para um diretório temporário e mover para não perder arquivos existentes
+      TMP_DIR=$(mktemp -d)
+      ${pkgs-unstable.git}/bin/git clone https://github.com/luiseduardobatista/pi.git "$TMP_DIR/repo"
+      ${pkgs-unstable.coreutils}/bin/cp -r "$TMP_DIR/repo/." "$PI_DOTFILES_DIR/"
+      ${pkgs-unstable.coreutils}/bin/chmod -R u+w "$PI_DOTFILES_DIR/"
+      ${pkgs-unstable.git}/bin/git -C "$PI_DOTFILES_DIR" remote set-url origin git@github.com:luiseduardobatista/pi.git
+      ${pkgs-unstable.coreutils}/bin/rm -rf "$TMP_DIR"
+      echo "Repositório PI clonado. Execute 'pi update' para reinstalar pacotes."
+    else
+      echo "O repositório PI já existe. Pulando clone."
+    fi
+  '';
 }
