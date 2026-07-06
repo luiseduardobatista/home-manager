054818fd76e8408224879ca91841df392f38914d
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-07-06 11:46:53 -0300

    remove useless code

--- nixos/desktops/niri.nix
+++ nixos/desktops/niri.nix
@@ -9,30 +9,4 @@
   ];
   programs.niri.enable = true;
   security.polkit.enable = true;
-
-  # Fix high VRAM usage on Nvidia
-  # environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text = ''
-  #   {
-  #     "rules": [
-  #       {
-  #         "pattern": {
-  #           "feature": "procname",
-  #           "matches": "niri"
-  #         },
-  #         "profile": "Limit Free Buffer Pool On Wayland Compositors"
-  #       }
-  #     ],
-  #     "profiles": [
-  #       {
-  #         "name": "Limit Free Buffer Pool On Wayland Compositors",
-  #         "settings": [
-  #           {
-  #             "key": "GLVidHeapReuseRatio",
-  #             "value": 0
-  #           }
-  #         ]
-  #       }
-  #     ]
-  #   }
-  # '';
 }
