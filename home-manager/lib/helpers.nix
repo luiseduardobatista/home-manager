0145d491e0fd4f06eab36fcfe1084e069892fa28
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-08-17 10:59:39 -0300

    refactor(noctalia): tornar config.toml v5 live out-of-store

--- home-manager/lib/helpers.nix
+++ home-manager/lib/helpers.nix
@@ -4,21 +4,10 @@
   repoDir,
   ...
 }: let
-  nixConfigPath = "${config.home.homeDirectory}/${repoDir}/home-manager";
   liveRepoPath = "${config.home.homeDirectory}/${repoDir}";
 in {
   _module.args = {
     inherit liveRepoPath;
-
-    linkApp = name: {
-      source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/programs/${name}/config";
-    };
-
-    linkFile = path: {
-      source = config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/${path}";
-    };
-
-    inherit nixConfigPath;
 
     gl = pkg:
       if isNixOS
--- modules/desktop/noctalia/default.nix
+++ modules/desktop/noctalia/default.nix
@@ -1,6 +1,14 @@
 {
-  flake.modules.homeManager.noctalia = {linkApp, ...}: {
+  flake.modules.homeManager.noctalia = {
+    config,
+    liveRepoPath,
+    ...
+  }: {
     programs.noctalia.enable = true;
-    xdg.configFile."noctalia" = linkApp "noctalia";
+
+    xdg.configFile."noctalia/config.toml".source =
+      config.lib.file.mkOutOfStoreSymlink "${liveRepoPath}/modules/desktop/noctalia/config.toml";
+
+    xdg.configFile."noctalia/palettes/kame-house-mc.json".source = ./palettes/kame-house-mc.json;
   };
 }
